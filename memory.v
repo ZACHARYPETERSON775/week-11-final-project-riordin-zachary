1b83ba3727f63c8311c26a8f7e678075654ed882
ZACHARYPETERSON775 <zacharypeterson05@u.boisestate.edu>
2024-11-01 10:12:14 -0600

    adding more files

---
+++ memory.v
@@ -0,0 +1,8 @@
+module memory(
+    input [7:0] data,
+    input store,
+    output reg [7:0] mem
+);
+
+    
+endmodule
---
+++ top.v
@@ -0,0 +1,10 @@
+module top(
+    input [15:0] sw,
+    input btnC, // Clock
+    input btnU, // Reset
+    output [15:0] led, // A & B display
+    output [6:0] seg,
+    output [3:0] an
+);
+
+endmodule
