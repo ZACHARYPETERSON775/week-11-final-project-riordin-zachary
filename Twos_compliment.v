658905254b2c2f0e7d60d72c499f967c5f85e9da
ZACHARYPETERSON775 <zacharypeterson05@u.boisestate.edu>
2024-11-13 22:15:11 -0700

    Implimented new method for generating a twos compliment

--- Twos_compliment.v
+++ Twos_compliment.v
@@ -2,9 +2,15 @@ module twos_comp(
     input [7:0] X,
     output [7:0] Y
 );
-    adder twos(
-        .A(~X),
-        .B(8'h01),
-        .Y(Y)
-    );
+    wire [7:0] C;
+    assign C[0] = X[0];
+    assign Y[0] = X[0];
+    
+    genvare i;
+    generate
+        for(i = 1; i < 8; i = i + 1) begin
+            assign C[i] = X[i] | C[i - 1];
+            assign Y[i] = X[i] ^ C[i - 1];
+        end
+    endgenerate
 endmodule
