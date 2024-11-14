ebf5f51219af3d55256b0e84490dac1b1f19d33c
ZACHARYPETERSON775 <zacharypeterson05@u.boisestate.edu>
2024-11-13 22:25:03 -0700

    Simplifed twos compliment even more

--- Twos_compliment.v
+++ Twos_compliment.v
@@ -2,14 +2,16 @@ module twos_comp(
     input [7:0] X,
     output [7:0] Y
 );
-    wire [7:0] C;
+    wire [6:0] C;
     assign C[0] = X[0];
     assign Y[0] = X[0];
-    
-    genvare i;
+
+    genvare i, Ci;
     generate
+	for(Ci = 1; Ci < 7; Ci = Ci + 1) begin
+            assign C[Ci] = X[Ci] | C[Ci - 1];
+        end
         for(i = 1; i < 8; i = i + 1) begin
-            assign C[i] = X[i] | C[i - 1];
             assign Y[i] = X[i] ^ C[i - 1];
         end
     endgenerate
